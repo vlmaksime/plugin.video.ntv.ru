a00f5540f632b06bfffe73aefdecb80e250fbb61
vlmaksime <vl.maksime@gmail.com>
2019-08-15 22:22:55 +0300

    Исправлена совместимость с Kodi 18

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
-# coding: utf-8
-# Module: tests
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function, unicode_literals
 import os
@@ -9,59 +8,75 @@ import imp
 import mock
 import shutil
 import xbmcaddon
-import xbmc
-
-addon_name = 'plugin.video.ntv.ru'
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-config_dir = os.path.join(cwd, 'config')
+
+addon_name = 'plugin.video.ntv.ru'
+sm_name = 'script.module.simplemedia'
+
+temp_dir = os.path.join(cwd, 'addon_data')
+
+if not os.path.exists(temp_dir):
+    os.mkdir(temp_dir)
+
+sm_dir = os.path.join(cwd, sm_name)
+sm_config_dir = os.path.join(temp_dir, sm_name)
+xbmcaddon.init_addon(sm_dir, sm_config_dir)
+
 addon_dir = os.path.join(cwd, addon_name)
-
-xbmcaddon.init_addon(addon_dir, config_dir, True)
-
-xbmc._set_log_level(-1)
+addon_config_dir = os.path.join(temp_dir, addon_name)
+xbmcaddon.init_addon(addon_dir, addon_config_dir, True)
 
 default_script = os.path.join(addon_dir, 'default.py')
+run_script = lambda : imp.load_source('__main__', default_script)
+
 
 # Import our module being tested
 sys.path.append(addon_dir)
 
 
 def tearDownModule():
-    shutil.rmtree(config_dir, True)
+
+    print('Removing temporary directory: {0}'.format(temp_dir))
+    shutil.rmtree(temp_dir, True)
 
 
 class PluginActionsTestCase(unittest.TestCase):
 
+    def setUp(self):
+
+        print("Running test: {0}".format(self.id().split('.')[-1]))
+
     @staticmethod
     @mock.patch('simpleplugin.sys.argv', ['plugin://{0}/'.format(addon_name), '1', ''])
     def test_01_root():
-        print('# test_root')
-        imp.load_source('__main__', default_script)
+
+        run_script()
 
     @staticmethod
-    @mock.patch('simpleplugin.sys.argv', ['plugin://{0}/genre/%D0%A1%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D1%8B'.format(addon_name), '2', ''])
+    @mock.patch('simpleplugin.sys.argv', ['plugin://{0}/genre/Сериалы'.format(addon_name), '2', ''])
     def test_02_genre():
-        print('# test_genre')
-        imp.load_source('__main__', default_script)
+
+        run_script()
 
     @staticmethod
     @mock.patch('simpleplugin.sys.argv', ['plugin://{0}/seasons/Beregovaya_ohrana'.format(addon_name), '3', ''])
     def test_03_seasons():
-        print('# test_seasons')
-        imp.load_source('__main__', default_script)
+
+        run_script()
 
     @staticmethod
     @mock.patch('simpleplugin.sys.argv', ['plugin://{0}/episodes/Beregovaya_ohrana/69020'.format(addon_name), '4', ''])
     def test_04_episodes():
-        print('# test_episodes')
-        imp.load_source('__main__', default_script)
+
+        run_script()
 
     @staticmethod
     @mock.patch('simpleplugin.sys.argv', ['plugin://{0}/video/829700'.format(addon_name), '5', ''])
-    def test_04_video():
-        print('# test_video')
-        imp.load_source('__main__', default_script)
+    def test_05_video():
+
+        run_script()
+
 
 
 if __name__ == '__main__':
